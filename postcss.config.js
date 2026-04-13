dbe59b144f5a6b9929bca8b1b6a76641a1ae45d9
Eliuzium <eliuzium@gmail.com>
2026-04-13 20:17:38 +0300

    Fix formatting and remove duplicate config lines

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,8 @@
-// postcss.config.js
+module.exports = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+};// postcss.config.js
 module.exports = {
   plugins: {
     "@tailwindcss/postcss": {},
